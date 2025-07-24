aff6da35473f9e99f0795600bf78326b5e5fcf28
ILtalim <isiaka.lawal@talim.ca>
2025-07-23 22:17:35 -0600

    update WF

--- main.tf
+++ main.tf
@@ -229,8 +229,8 @@ resource "aws_launch_template" "prod_lnch_tmpl" {
 resource "aws_autoscaling_group" "prod_asg" {
   name                      = "${local.name}-prod-asg"
   max_size                  = 3
-  min_size                  = 1
-  desired_capacity          = 1
+  min_size                  = 2
+  desired_capacity          = 2
   health_check_grace_period = 120
   health_check_type         = "EC2"
   force_delete              = true
